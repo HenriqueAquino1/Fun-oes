b69f5041a970898f9d69df73bc67168511b0c181
Henrique Aquino <rickybrasileiro1@gmail.com>
2023-02-02 10:57:38 -0300

    Funcoes

---
+++ funcoes.js
@@ -0,0 +1,70 @@
+
+/*
+function retornaValor() {
+  return 1;
+}
+console.log(retornaValor());
+
+ Função de retorno */
+
+
+
+/*
+let show = function() {
+
+console.log ('Anonymous function');
+
+};
+
+show();
+
+ Função Anonima 
+
+
+function precos(mouse,teclado){
+
+  let totalItens = mouse + teclado
+  console.log("Novo Preço: "+totalItens)
+
+}
+
+precos(35,100)
+
+
+function teste(a,b){
+return console
+
+}
+
+
+
+
+let soma = (x,y)=> x + y
+console.log(soma(10,50))
+
+let nameletters = (name) => name.lenght
+console.log(nameletters('Josias'))
+
+
+
+
+const dollar = (real) => real / 5.02;
+console.log(dollar(10));*/
+
+
+
+function maior(a,b){
+if (a > b){
+  console.log(`O numero ${a} é maior`)
+   } else{
+
+    console.log (`O numero ${b} é maior`)
+
+  }
+
+}
+  maior(25,40)
+
+
+
+    
